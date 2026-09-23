88bb95a178d086531ae26a848c3f8c024fb86172
vaidusdp <vaidus2608m@gmail.com>
2026-09-23 20:05:40 +0530

    initializing backend with supabase and drizzel

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,15 @@
+import {
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({ length: 255 }).notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp().notNull().defaultNow(),
+})
